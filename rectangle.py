5a638c83658234c92587d1c69a33bb7a94a849f2
First work <pty997355165@outlook.com>
2023-08-26 22:25:01 +0300

    third commit

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+# Caculate the length and width of rectangle
+import math
+
+length = int(input("Enter the lenth of the rectangle:"))
+width = int(input("Enter the width of the rectangle:"))
+perimeter = (length + width) * 2
+print(f"perimeter of the rectangle: {perimeter}")
+area = length * width
+print(f"Area of the rectangle: {area}")
