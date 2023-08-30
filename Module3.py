d4de1cd00720cff593c9c9ac9095ddd27d5781ba
First work <pty997355165@outlook.com>
2023-08-30 17:48:14 +0300

    module3_commit

--- Module3.py
+++ Module3.py
@@ -17,7 +17,33 @@ elif (cabin == "A"):
     print("above the car deck, equipped with a window")
 elif(cabin == "B"):
     print("windowless cabin above the car deck")
+elif(cabin == "C"):
+    print("windowless cabin below the car deck")
 else:
-    print(" windowless cabin below the car deck")
+    print("Invalid cabin class")
 
 
+#3
+import math
+gender = str(input("Please enter your biological gender:"))
+hemoglobin = int(input("Please enter your hemoglobin value (g/l):"))
+if (117 <= hemoglobin <= 155 and gender == "females"):
+    print("The hemoglobin value is normal.")
+elif(hemoglobin < 117 and gender == "females"):
+    print("The hemoglobin value is low.")
+elif(hemoglobin > 155 and gender == "females"):
+    print("The hemoglobin value is high.")
+if(134 <= hemoglobin <= 167 and gender == "males"):
+    print("The hemoglobin value is normal.")
+elif(hemoglobin < 134 and gender == "males"):
+    print("The hemoglobin value is low.")
+elif(hemoglobin > 167 and gender == "males"):
+    print("The hemoglobin value is high.")
+
+
+#4
+year = int(input("Enter a year:"))
+if(year/4==0 or year/400==0):
+    print("That is a leap year.")
+else:
+    print("That is not a leap year.")
