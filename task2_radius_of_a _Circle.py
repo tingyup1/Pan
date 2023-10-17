fca3a9bba9b6ca27b72dc4632ba6c51b21a8f54f
tingyup1 <142997103+tingyup1@users.noreply.github.com>
2023-10-17 22:31:18 +0300

    Delete task2_radius_of_a _Circle.py

--- task2_radius_of_a _Circle.py
+++
@@ -1,6 +0,0 @@
-import math
-pi = 3.1415926
-formatPI = ("value of PI: {:.2f}".format(math.pi))
-radius = float(input("Enter the radius:"))
-areaCircle = math.pi * radius ** 2
-print(f"Area of the circle:{areaCircle:.2f} ")
